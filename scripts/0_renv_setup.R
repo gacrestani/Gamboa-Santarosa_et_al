3d0d0dcec3c6ae62f4ecb038da27b1c35823e540
Giovanni A. Crestani <gacrestani@gmail.com>
2025-08-26 13:27:01 -0700

    Updated README.md

--- scripts/0_renv_setup.R
+++ scripts/0_renv_setup.R
@@ -6,3 +6,11 @@ renv::install("bioc::org.Dm.eg.db")
 renv::install("bioc::TxDb.Dmelanogaster.UCSC.dm6.ensGene")
 renv::install("MartaPelizzola/ACER")
 renv::install("ThomasTaus/poolSeq")
+
+
+# Also, run on debian:
+sudo apt install libcurl4-openssl-dev \
+libxml2-dev \
+libfontconfig1-dev \
+libglpk-dev 
+
